6adf9de82a9c2ed0be6fcfb25b9fb3aa7f025625
bsosa <gabossosa@gmail.com>
2025-05-18 15:20:36 -0600

    configuracion colores y tema oscuro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,7 @@
 <template>
   <main>
     <h1 class="bg-red-200 text-2xl">Home</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias molestiae accusantium qui magnam mollitia eum
+      temporibus accusamus aut dolor nobis?</p>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     extend: {},
+    colors: {
+      primary: '#1E3A8A',
+      secondary: '#FBBF24',
+      accent: '#F472B6',
+      neutral: '#374151',
+      'base-100': '#FFFFFF',
+      info: '#3ABFF8',
+      success: '#36D399',
+      warning: '#FBBD23',
+      error: '#F87272',
+    },
   },
   plugins: [],
 }
