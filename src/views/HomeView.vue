58768fc8730700d6452ca7984351936a4c446ee8
bsosa <gabossosa@gmail.com>
2025-05-18 14:53:16 -0600

    init files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="bg-red-200 text-2xl">Home</h1>
   </main>
 </template>
